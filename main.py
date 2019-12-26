97534fd60f9b8dd87e07177f0258ac306dc8aa1e
William Reed <wreed58@gmail.com>
2019-12-26 15:05:22 -0500

    add argument and print out for other fees

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from requests.auth import HTTPBasicAuth
 
 parser = argparse.ArgumentParser(description="Generate toggl report for the past two weeks ending at EOD yesterday")
 parser.add_argument("--rate", "-r", type=float, help="hourly rate", required=True)
+parser.add_argument("--additional", "-a", type=float, help="additional fees per pay period", required=False)
 args = parser.parse_args()
 RATE = args.rate
+ADDITIONAL_FEES = args.additional
 
 REPORTS_URL = "https://toggl.com/reports/api/v2/details"
 WORKSPACE_URL = "https://www.toggl.com/api/v8/workspaces"
@@ -49,5 +51,9 @@ hours = report["total_grand"] / 3_600_000  # convert ms to hours
 
 print("Report for {} UTC to {} UTC".format(start, end))
 print("Hours:\t{:.2f}".format(hours))
-print("Rate:\t{:.2f}/hr".format(RATE))
-print("Pay:\t${:.2f}".format(hours * RATE))
+print("Rate:\t${:.2f}/hr".format(RATE))
+if ADDITIONAL_FEES is not None:
+    print("Other:\t${:.2f}".format(ADDITIONAL_FEES))
+else:
+    ADDITIONAL_FEES = 0
+print("Pay:\t${:.2f}".format(hours * RATE + ADDITIONAL_FEES))
